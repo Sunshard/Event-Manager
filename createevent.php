3b81c1321208862e98319d06a35e2a492b1f3a60
unknown <Sunshard@.(none)>
2010-07-15 09:27:06 +0800

    Added javascript, updated user requirements.

---
+++ createevent.php
@@ -0,0 +1,11 @@
+<?php
+	$user=;
+	$pass=;
+	/* Connection to Database here (using example code for a PDO connection) */
+	// $avantusDb = new PDO('mysql:host=localhost;dbname=Events', $user, $pass);
+	
+	// Example of comprec's (uses mysqli not PDO):  new mysqli("mysql.hjmills.co.uk", "comprec", "dan1el", "hjmills_comprec");
+
+	
+
+?>
--- createform.php
+++ createform.php
@@ -1,27 +1,56 @@
-<?php // Type number of fields correctly for the table
-	$fieldQty = (int)$_POST['fieldQty'];
-	$user=;
-	$pass=;
-	/* Connection to Database here (using example code for a PDO connection) */
-	// $avantusDb = new PDO('mysql:host=localhost;dbname=Events', $user, $pass);
-	// Example of comprec's (uses mysqli not PDO):  new mysqli("mysql.hjmills.co.uk", "comprec", "dan1el", "hjmills_comprec");
-
+<?php
+	// Initialise number of base fields to 10. This limit can be altered with the variable below.
+	$fieldQty = 10;
+	
+	// Start document, on-page Javascript included below.
+	echo '
+		<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"> 
+		<html>
+		<title>Event Form Design</title>	
+		
+		<head>
+		<script type="text/javascript"> // Note: If we want PHP to modify the script, it may be necessary to have this in-place on the page.
+		<!--		
+			function enableDataEntry()
+			{ // Initialise (Poor discipline, determine if there is a better way to initialise)
+			var enableArray = document.getElementsbyName(isEnabled);
+			// Warning: Need to pass by reference not by value. Ensure this is the case.
+			var labelArray = document.getElementsbyName(label);
+			var typeArray = document.getElementsbyName(dataType);
+			
+			for(var i = 1; i < $fieldQty; i++)
+			{
+				if(enableArray[i].checked) // Determine if this works in Javascript
+				{ // Need to pass by reference. Check if this succeeds.
+					labelArray[i].disabled="false";
+					typeArray[i].disabled="false";
+				}
+				else { // Assume it has been unchecked.
+					labelArray[i].disabled="true";
+					typeArray[i].disabled="true";
+				}
+			}
+		// -->
+		</script>
+		</head>';
+	
 	/*
 	Print table header. Contents include:
-	- field label
-	- enabled (Using a checkbox, with the tick present if yes).
-	- data type **Determine if necessary / desirable
+	- Enabled:- Marks whether the field is in use (Using a checkbox, with the tick present if yes).
+	- Field Label:- The Description of the field the users see 	
+	- Data type:- An archetype of the field's contents; used for mapping to the backend database.
 	
-	To Do: Determine if we can get an overriding css for the table. If so, overwrite border
+	To Do: 
+	- Determine if we can get an overriding css for the table. If so, overwrite border
+	- css should also replace the 'align' property in th as it is deprecated.
 	*/
-
-	//! Determine how to encapsulate table correctly as another submission form
 	
-	echo '<form action="eventpreview.php" method="post">
+	echo	'<body>
+	
+			<form action="formReview.php" method="post">
 
 			<table border="1">
 			<tr>
-				<th 
 				<th  align="center" valign="middle" scope="col">Enabled? <small>(Yes/No)</small></th>
 				<th  align="center" valign="middle" scope="col">Field Label</th>
 				<th  align="center" valign="middle" scope="col">Data Type</th>
@@ -30,21 +59,38 @@
 
 	for($i = 1; $i < $fieldQty; $i++) 
 	{	/* Looping production of input fields.
-		Data types currently limited to String, Number and Date.
+		
+		Notes: 
+		-	Determine if tabindex functions.
+		-	Check that the javascript works
+		
+		Description of Data Types:
+		
+		Customer Name (CName) - 
+		Salutation - 
+		Company Name - 
+		Designation
+		Email
+		Custom
 		*/
 
 		echo '<tr>
-				<td><input type="checkbox" name="isEnabled$fieldQty" /></td>
-				<td><input type="text" name="label$fieldQty" /></td>
-				<td><select name="dataType$fieldQty">
-						<option value="String" selected="selected">String</option>
-						<option value="Number">Number</option>
-						<option value="Date">Date</option>
+				<td><input type="checkbox" name="isEnabled" tabindex=(3*$i - 2)/></td>
+				<td><input type="text" name="label" tabindex=(3*$i - 1)/></td>
+				<td><select name="dataType" tabindex= 3*$i>
+						<option value="dataTypeCName" selected="selected">Customer Name</option>
+						<option value="dataType">Salutation</option>
+						<option value="dataTypeDesignation">Designation</option>
+						<option value="dataTypeCompany">Company Name</option>
+						<option value="dataTypeEmail">Email</option>
+						<option value="dataTypeCustom">Custom</option>
 					</select></td>
 			</tr>';
 	}
 	
 	echo '</table>
 	<br><input type="submit" />
-	</form>';
+	</form>
+	</body>
+	</html>';
 ?>
